d4e04286b3d7af27b93f6b4db96511698f25dbf7
StefanBRas <gitcommits@bruhn.io>
2023-06-20 11:49:46 +0200

    cli from all import types

--- src/polugins/main.py
+++ src/polugins/main.py
@@ -19,40 +19,21 @@ else:
     import importlib_metadata  # ignore
 
 
-def get_entrypoints():
-    entry_points = {
-        ExtensionClass.EXPR: [],
-        ExtensionClass.SERIES: [],
-        ExtensionClass.LAZYFRAME: [],
-        ExtensionClass.DATAFRAME: [],
-    }
-    for dist in list(importlib_metadata.distributions()):
-        for ep in dist.entry_points:
-            if ep.group.startswith("polugins"):
-                entry_points[ExtensionClass.from_entrypoint_group(ep.group)].append(ep)
-    return entry_points
-
-
-def load_setuptools_entrypoints():
-    """
-    TODO: Stolen from pluggy, remember license and stuff
-    """
-    all_entry_points = get_entrypoints()
+def _get_entrypoint_namespaces():
     namespaces = {
         ExtensionClass.EXPR: {},
         ExtensionClass.SERIES: {},
         ExtensionClass.LAZYFRAME: {},
         ExtensionClass.DATAFRAME: {},
     }
-    for namespace_type, entry_points in all_entry_points.items():
-        for ep in entry_points:
+    for dist in list(importlib_metadata.distributions()):
+        for ep in dist.entry_points:
             if ep.group.startswith("polugins"):
-                namespace = ep.load()
-                namespaces[namespace_type][ep.name] = namespace
-    return namespaces
+                extension_class = ExtensionClass.from_entrypoint_group(ep.group)
+                namespaces[extension_class][ep.name] = ep.value
+    return namespaces 
 
-
-def load_config_namespaces():
+def _get_config_namespaces():
     all_namespaces = {
         ExtensionClass.EXPR: {},
         ExtensionClass.SERIES: {},
@@ -60,26 +41,23 @@ def load_config_namespaces():
         ExtensionClass.DATAFRAME: {},
     }
 
-    def _load_from_toml(toml: dict):
+    def _get_from_toml(toml: dict):
         for namespace_type, namespaces in toml.items():
             extension_class = ExtensionClass(namespace_type)
             for name, namespace_path in namespaces.items():
-                module_path, namespace_object_name = namespace_path.split(":")
-                namespace_module = importlib.import_module(module_path)
-                namespace = getattr(namespace_module, namespace_object_name)
-                all_namespaces[extension_class][name] = namespace
+                all_namespaces[extension_class][name] = namespace_path
 
     if (polugins_toml := Path("polugins.toml")).exists():
         toml = tomllib.loads(polugins_toml.read_text())
-        _load_from_toml(toml)
+        _get_from_toml(toml)
     if (pyproject := Path("pyproject.toml")).exists():
         pyproject_toml = tomllib.loads(pyproject.read_text())
         toml = pyproject_toml.get("tool", {}).get("polugins")
         if toml:
-            _load_from_toml(toml)
+            _get_from_toml(toml)
     return all_namespaces
 
-def load_env_namespaces():
+def _get_env_namespaces():
     all_namespaces = {
         ExtensionClass.EXPR: {},
         ExtensionClass.SERIES: {},
@@ -88,18 +66,41 @@ def load_env_namespaces():
     }
     for env_var_name,env_var_value in os.environ.items():
         if env_var_name.casefold().startswith("polugins".casefold()):
-            print("here now")
             _, extension_class, name = env_var_name.split("_")
             extension_class = ExtensionClass(extension_class)
-            module_path, namespace_object_name = env_var_value.split(":")
-            namespace_module = importlib.import_module(module_path)
-            namespace = getattr(namespace_module, namespace_object_name)
-            all_namespaces[extension_class][name] = namespace
-
+            all_namespaces[extension_class][name] = env_var_value
     return all_namespaces
 
 
 NamespaceDict = Dict[str, Union[Type, str]]
+
+def _get_namespaces(
+    load_entrypoints: bool = True,
+    load_config: bool = True,
+    load_env: bool = True,
+    ):
+    all_namespaces = {
+        ExtensionClass.EXPR: {},
+        ExtensionClass.SERIES: {},
+        ExtensionClass.LAZYFRAME: {},
+        ExtensionClass.DATAFRAME: {},
+    }
+    if load_entrypoints:
+        ep_namespaces = _get_entrypoint_namespaces()
+        for extension_class, namespaces in ep_namespaces.items():
+            for name, namespace in namespaces.items():
+                all_namespaces[extension_class][name] = namespace
+    if load_config:
+        config_namespaces = _get_config_namespaces()
+        for extension_class, namespaces in config_namespaces.items():
+            for name, namespace in namespaces.items():
+                all_namespaces[extension_class][name] = namespace
+    if load_env:
+        config_namespaces = _get_env_namespaces()
+        for extension_class, namespaces in config_namespaces.items():
+            for name, namespace in namespaces.items():
+                all_namespaces[extension_class][name] = namespace
+    return all_namespaces
 
 
 def register_namespaces(
@@ -120,18 +121,7 @@ def register_namespaces(
         ExtensionClass.EXPR.register(name, namespace)
     for name, namespace in series_namespaces.items():
         ExtensionClass.SERIES.register(name, namespace)
-    if load_entrypoints:
-        ep_namespaces = load_setuptools_entrypoints()
-        for extension_class, namespaces in ep_namespaces.items():
-            for name, namespace in namespaces.items():
-                extension_class.register(name, namespace)
-    if load_config:
-        config_namespaces = load_config_namespaces()
-        for extension_class, namespaces in config_namespaces.items():
-            for name, namespace in namespaces.items():
-                extension_class.register(name, namespace)
-    if load_env:
-        config_namespaces = load_env_namespaces()
-        for extension_class, namespaces in config_namespaces.items():
-            for name, namespace in namespaces.items():
-                extension_class.register(name, namespace)
+    other_namespaces = _get_namespaces(load_entrypoints, load_config, load_env)
+    for extension_class, namespaces in other_namespaces.items():
+        for name, namespace in namespaces.items():
+            extension_class.register(name, namespace)
