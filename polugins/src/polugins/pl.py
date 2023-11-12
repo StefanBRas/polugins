265f08ba2b9529ed2985dbce8a2a39209a1440c7
StefanBRas <gitcommits@bruhn.io>
2023-11-12 20:05:00 +0100

    loosen deps

--- polugins/src/polugins/pl.py
+++ polugins/src/polugins/pl.py
@@ -3,3 +3,4 @@ from polars import *  # noqa: F403
 from polugins.main import register_namespaces as _register_namespaces
 
 _register_namespaces(load_entrypoints=True, load_config=True, load_env=True)
+
