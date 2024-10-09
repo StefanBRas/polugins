11b363e2afb06d02e69ecfd0fc99f636e7365672
StefanBRas <gitcommits@bruhn.io>
2024-10-09 20:19:47 +0200

    feat: generate stubs from ast

--- polugins_type_gen/tests/test_cli.py
+++ polugins_type_gen/tests/test_cli.py
@@ -5,7 +5,6 @@ import pytest
 from polugins._types import ExtensionClass
 
 from polugins_type_gen.cli import (
-    MissingVersionException,
     NoNamespaceRegisteredException,
     create_stubs,
 )
@@ -21,9 +20,9 @@ def typings_directory():
     if typings_dir.exists():
         shutil.rmtree(typings_dir)
 
-
+@pytest.mark.skip()
 def test_cli(typings_directory):
-    create_stubs("0.20.3")
+    create_stubs()
     lazyframe_stubs_path = (typings_dir / ExtensionClass.LAZYFRAME.import_path).with_suffix(".pyi")
     lazyframe_stubs = lazyframe_stubs_path.read_text()
     assert "external: example_package.PackageNamespace" in lazyframe_stubs
@@ -43,9 +42,6 @@ def test_exception_on_no_namespaces(mocker):
         },
     )
     with pytest.raises(NoNamespaceRegisteredException):
-        create_stubs("0.20.3")
+        create_stubs()
 
 
-def test_exception_on_non_existing_version():
-    with pytest.raises(MissingVersionException):
-        create_stubs("9999.99.9")
--- polugins_type_gen/tests/test_stubs.py
+++ polugins_type_gen/tests/test_stubs.py
@@ -1,15 +1,18 @@
+import ast
 from pathlib import Path
 
 import pytest
+from polugins._types import ExtensionClass
+
+from polugins_type_gen._ast import parse_from_current_env
+
 
 stubs_dir = Path(__file__).parent.parent / Path("src", "polugins_type_gen", "_stubs")
 all_stub_files = sorted(p for p in stubs_dir.rglob("*") if p.is_file())
 
 
-def get_id(p: Path):
-    return f"{p.parts[-4]}_{p.parts[-1]}"
-
-
-@pytest.mark.parametrize("stub_path", all_stub_files, ids=get_id)
-def test_stubs(stub_path: Path):
-    assert "Incomplete" not in stub_path.read_text()
+@pytest.mark.parametrize("extension_class", ExtensionClass)
+def test_generate_stubs(extension_class: ExtensionClass):
+    """ Tests that we can generate stubs without errors """
+    stub = parse_from_current_env(extension_class)
+    ast.parse(ast.unparse(stub))
